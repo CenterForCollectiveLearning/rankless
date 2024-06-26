0b04a33b39a329441373bbb505e148a2a91dfb68
Endre Márk Borza <endremborza@gmail.com>
2024-06-26 11:12:42 +0200

    new data backend

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,8 @@
 export const APP_NAME = 'Rankless';
-const LOCAL = true;
-const LOCAL_LINK = 'http://0.0.0.0:8000'
-// const LOCAL_LINK = 'https://stork-settled-spider.ngrok-free.app'
-export const STORE_URL = LOCAL ? LOCAL_LINK : 'https://tmp-borza-public-cyx.s3.amazonaws.com/quercus-basis-v2024-05-29';
+const LOCAL = false;
+// const LOCAL_LINK = 'http://0.0.0.0:8000'
+const LOCAL_LINK = 'https://stork-settled-spider.ngrok-free.app'
+export const STORE_URL = LOCAL ? LOCAL_LINK : 'https://tmp-borza-public-cyx.s3.amazonaws.com/quercus-basis-v2024-06-26';
 
 export const INSTITUTION_TYPE = 'institutions';
 
